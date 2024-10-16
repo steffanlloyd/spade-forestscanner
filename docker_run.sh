30a6a02fe92116ae267e79a6aa359a66f1392b27
Steffan Lloyd <steffan.lloyd@nibio.no>
2024-10-16 16:27:47 +0200

    updates

--- docker_run.sh
+++ docker_run.sh
@@ -3,9 +3,11 @@ docker run -itd \
   --rm \
   --privileged --network host --ipc host --user ros \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /home/spade/ros1/ros1_ws:/home/ros/ros1_ws \
-  -v /home/spade/rosbags:/home/ros/rosbags \
+  -v $(dirname "$0")/ros1_ws:/home/ros/ros1_ws \
+  -v $(dirname "$0")/rosbags:/home/ros/rosbags \
   -e DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   forestscanner-ros1:latest \
   tail -f /dev/null
+
+docker ps
